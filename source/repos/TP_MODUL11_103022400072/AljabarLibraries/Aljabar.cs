4988c933a35d92caf601fb01564aae0a7ad66647
napisang <nafisazahira2773@gmail.com>
2026-05-10 22:41:13 +0700

    implementasi AljabarLibraries dan Console App

---
+++ source/repos/TP_MODUL11_103022400072/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,32 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = (b * b) - (4 * a * c);
+
+            if (discriminant < 0)
+                throw new Exception("Persamaan tidak memiliki akar real.");
+
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+            if (x1 < x2) { double temp = x1; x1 = x2; x2 = temp; }
+
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            return new double[] { a * a, 2 * a * b, b * b };
+        }
+    }
+}   
---
+++ source/repos/TP_MODUL11_103022400072/TP_MODUL11_103022400072/Program.cs
@@ -0,0 +1,12 @@
+﻿using AljabarLibraries;
+
+Console.WriteLine("=== ALJABAR LIBRARIES ===\n");
+
+double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+Console.WriteLine("AkarPersamaanKuadrat({1, -3, -10})");
+Console.WriteLine($"Output: {{{string.Join(", ", akar)}}}");
+
+Console.WriteLine();
+double[] hasil = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+Console.WriteLine("HasilKuadrat({2, -3})");
+Console.WriteLine($"Output: {{{string.Join(", ", hasil)}}}");
